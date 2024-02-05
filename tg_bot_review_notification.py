8aa123657851368d2adeb4448f451a6bc898d731
BigBadRussian <skripkoae@gmail.com>
2024-02-05 13:02:49 +0400

    no timestamp update func; if response == found now in func main, launch send msg bot and update timestamp

--- tg_bot_review_notification.py
+++ tg_bot_review_notification.py
@@ -13,10 +13,10 @@ def set_cli_args(default_chat_id):
     return parser
 
 
-def get_devman_reviews(timestamp, devman_token):
+def get_devman_reviews(devman_token, params):
     url = 'https://dvmn.org/api/long_polling/'
-    headers = {'Authorization': f'Token {devman_token}', 'timestamp': timestamp}
-    response = requests.get(url, headers=headers, timeout=90)
+    headers = {'Authorization': f'Token {devman_token}'}
+    response = requests.get(url, headers=headers, params=params, timeout=90)
     response.raise_for_status()
     return response.json()
 
@@ -28,19 +28,12 @@ def prepare_text_for_tlg_message(response, task_name):
 
 
 def send_message_on_server_reply(response, bot, chat_id):
-    if response['status'] == 'found':
-        task_name = response['new_attempts'][0]['lesson_title']
-        text = prepare_text_for_tlg_message(response=response, task_name=task_name)
-        bot.send_message(text=text, chat_id=chat_id)
+    task_name = response['new_attempts'][0]['lesson_title']
+    text = prepare_text_for_tlg_message(response=response, task_name=task_name)
+    bot.send_message(text=text, chat_id=chat_id)
 
 
-def update_timestamp_for_next_request(response):
-    if response['status'] == 'found':
-        return str(response['last_attempt_timestamp'])
-    return str(response['timestamp_to_request'])
-
-
-def launch_bot():
+def main():
     load_dotenv()
     devman_token = os.environ['DEVMAN_TOKEN']
     tg_bot_token = os.environ['TG_BOT_TOKEN']
@@ -48,21 +41,20 @@ def launch_bot():
     bot = telegram.Bot(tg_bot_token)
     cli_args = set_cli_args(default_chat_id=default_chat_id).parse_args()
     chat_id = cli_args.chat_id
-
-    response = get_devman_reviews(devman_token=devman_token, timestamp=None)
-    send_message_on_server_reply(response=response, bot=bot, chat_id=chat_id)
-    timestamp = update_timestamp_for_next_request(response=response)
-    response = get_devman_reviews(devman_token=devman_token, timestamp=timestamp)
-    send_message_on_server_reply(response=response, bot=bot, chat_id=chat_id)
-    update_timestamp_for_next_request(response=response)
-
-
-if __name__ == '__main__':
+    params = {}
     while True:
         try:
-            launch_bot()
+            response = get_devman_reviews(devman_token=devman_token, params=params)
+            if response['status'] == 'found':
+                params['timestamp'] = str(response['last_attempt_timestamp'])
+                send_message_on_server_reply(response=response, bot=bot, chat_id=chat_id)
         except requests.exceptions.ReadTimeout as timeout_error:
             print(timeout_error)
         except requests.exceptions.ConnectionError as connection_error:
             print(connection_error)
             time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
+
